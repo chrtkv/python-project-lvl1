11d50c363025afba0e41ab9043b1938af072a242
Kirill Chertkov <chertkov.k@gmail.com>
2020-12-18 09:36:48 +0300

    refactor gcd game module

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,23 +1,12 @@
+import math
 import random
 
 RULES = 'Find the greatest common divisor of given numbers.'
-
-
-def gcd(m, n):
-    divisor = n if m > n else m
-
-    def find_gcd(m, n, divisor):
-        if m % divisor == 0 and n % divisor == 0:
-            return divisor
-        else:
-            return find_gcd(m, n, divisor - 1)
-
-    return (find_gcd(m, n, divisor))
 
 
 def gcd_game():
     m = random.randint(1, 100)
     n = random.randint(1, 100)
     question = f'{m} {n}'
-    answer = gcd(m, n)
+    answer = math.gcd(m, n)
     return (question, str(answer))
