d00afeab9bc6f2875a668f71d7f9a512f4c7c3ef
Kirill Chertkov <chertkov.k@gmail.com>
2020-12-25 09:40:55 +0300

    change random.choice to random.randint

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -6,11 +6,10 @@ DESCRIPTION = 'What number is missing in the progression?'
 def generate_round():
     start = random.randint(1, 100)
     step = random.randint(2, 10)
-    # size of progression — from 5 to 10 numbers
-    size = random.choice(range(5, 11))
+    size = random.randint(5, 10)
     progression = list(range(start, start + step * size, step))
 
-    omitted_number_index = random.choice(range(0, size))
+    omitted_number_index = random.randint(0, size - 1)
     answer = progression[omitted_number_index]
     question = ' '.join(['..' if i == answer else str(i) for i in progression])
 
